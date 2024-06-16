7d1c3df67ed8d50e721151eb7193574c9ef4c047
TejasDiyora <TejaskumarVdiyora@gmail.com>
2024-06-16 08:19:59 -0800

    Create 2. InputOutput2.c

---
+++ 2. InputOutput2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main(){
+    printf("#####");
+    printf("\n#");
+    printf("\n#");
+    printf("\n####");
+    printf("\n#");
+    printf("\n#");
+    printf("\n#");
+    printf("\n");
+    printf("\n\t#####");
+    printf("\n ##       ##");
+    printf("\n#");
+    printf("\n#");
+    printf("\n#");
+    printf("\n#");
+    printf("\n#");
+    printf("\n ##       ##");
+    printf("\n\t#####");
+    
+}
