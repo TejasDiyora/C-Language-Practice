feb28cdb34d8291effbc76bd684294196d5c4608
TejasDiyora <TejaskumarVdiyora@gmail.com>
2024-06-17 07:37:01 -0800

    Create 5. AreaPerimeter.c
    Area and Perimeter of Rectangle

---
+++ 5. AreaPerimeter.c
@@ -0,0 +1,16 @@
+//Write a C program to compute the perimeter and area of a rectangle.
+
+#include<stdio.h>
+
+void main(){
+    int length, width, area, perimeter;
+    
+    printf("Enter a length and width of rectangle: ");
+    scanf("%d %d",&length, &width);
+    
+    perimeter = 2 * (length + width);
+    area = length * width;
+    
+    printf("Area is : %d",area);
+    printf("\nPerimeter is : %d",perimeter);
+}
