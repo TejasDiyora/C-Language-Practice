0a894f9cbad0af5f10e35457da4f1044917034c3
TejasDiyora <TejaskumarVdiyora@gmail.com>
2024-07-26 13:03:16 -0700

    Create 078. SquareDesign78.c

---
+++ 078. SquareDesign78.c
@@ -0,0 +1,31 @@
+// Write a C program that reads the side (side sizes between 1 and 10 ) of a square and prints square using hash (#) character.
+// Sample Input: 10
+// Sample Output:
+
+// Input the size of the square: 
+//  # # # # # # # # # #
+//  # # # # # # # # # #
+//  # # # # # # # # # #
+//  # # # # # # # # # #
+//  # # # # # # # # # #
+//  # # # # # # # # # #
+//  # # # # # # # # # #
+//  # # # # # # # # # #
+//  # # # # # # # # # #
+//  # # # # # # # # # #
+
+#include<stdio.h>
+
+int main(){
+    int i,j,size;
+    
+    printf("Enter a size of the square: ");
+    scanf("%d",&size);
+    
+    for(i=1;i<=size;i++){
+        for(j=1;j<=size;j++){
+            printf("# ");
+        }
+        printf("\n");
+    }
+}
